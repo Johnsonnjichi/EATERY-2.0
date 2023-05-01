f0d54cd29b126e3e686a667056cf2f39a64f2be1
johnson <johnsonnjichi@gmail.com>
2023-05-01 23:16:36 +0300

    single item display

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from 'react'
 
 
-
-
 function Menus() {
   const [menus, setMenus] = React.useState([]);
+  const [selectedMenu, setSelectedMenu] = React.useState(null);
 
   React.useEffect(() => {
     fetch("http://localhost:4300/menus")
@@ -12,16 +11,57 @@ function Menus() {
       .then((menus) => setMenus(menus));
   }, []);
 
+  function renderSingleMenu(menuname) {
+    const menu = menus.find((mn) => mn.title === menuname);
+    setSelectedMenu(menu);
+  }
+
   return (
-    <ul id="menus">
-      {menus.map((menu) => (
-        <li key={menu.title}>{menu.title}</li>
-      ))}
-    </ul>
+    <>
+      <ul id="menus">
+        {menus.map((menu) => (
+          <li key={menu.title} onClick={() => renderSingleMenu(menu.title)}>
+            {menu.title}
+          </li>
+        ))}
+      </ul>
+      {selectedMenu && (
+        <div>
+          <img src={selectedMenu.image} alt={selectedMenu.title} />
+          <h2>{selectedMenu.title}</h2>
+          <p>{selectedMenu.price}</p>
+          <p>
+            {selectedMenu.order_capacity - selectedMenu.orders_received} orders
+            left
+          </p>
+          <button id="place-order">Place Order</button>
+        </div>
+      )}
+    </>
   );
-
-
 }
+
+
+
+// function Menus() {
+//   const [menus, setMenus] = React.useState([]);
+
+//   React.useEffect(() => {
+//     fetch("http://localhost:4300/menus")
+//       .then((res) => res.json())
+//       .then((menus) => setMenus(menus));
+//   }, []);
+
+//   return (
+//     <ul id="menus">
+//       {menus.map((menu) => (
+//         <li key={menu.title}>{menu.title}</li>
+//       ))}
+//     </ul>
+//   );
+
+
+// }
 
     
 
